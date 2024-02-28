7e9e9b049c9e5d17756bb27b60ddde6f029d5fef
rafael <rafaondjango@gmail.com>
2024-02-28 19:54:04 -0300

    feat: esbocoço da API

---
+++ SpaceBlogApi/Apps/Post/Controllers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaceBlogApi.Apps.Post;
+
+
+[ApiController]
+[Route("/HelloWorld")]
+public class Controllers
+{
+	[HttpGet]
+	public ActionResult<string> SayHelloWorld()
+	{
+		return "Hello World!";
+	}
+}
---
+++ SpaceBlogApi/Apps/Post/Model.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceBlogApi.Apps.Post;
+
+public class Model
+{
+	public int Id { get; set; }
+	public string Text { get; set; } = string.Empty;
+	public byte[]? Image { get; set; }
+}
