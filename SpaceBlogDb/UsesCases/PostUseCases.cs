3b1436fe363130067a1b4d047d9cb717f29a86aa
rafael <rafaondjango@gmail.com>
2024-03-05 10:00:32 -0300

    feat(spaceblogdb): configurando caso de uso para criação de um post no banco de dados

---
+++ SpaceBlogDb/UsesCases/PostUseCases.cs
@@ -0,0 +1,17 @@
+﻿using SpaceBlogDb.Models;
+using SpaceBlogDb.Repositories;
+
+namespace SpaceBlogDb.UsesCases;
+
+public class PostUseCases
+{
+	public static async Task Create(Post post)
+	{
+		CustomDbContext dbContext = new();
+		PostsRepository postsRepository = new(dbContext);
+
+		postsRepository.Add(post);
+
+		await dbContext.SaveChangesAsync();
+	}
+}
