7344a4c033178dc07cc740cba5fcc26bf4653c7c
rafael <rafaondjango@gmail.com>
2024-03-05 15:43:21 -0300

    feat(spaceblog): finalizando a publicação de imagem no sistema

--- SpaceBlogApi/Apps/Post/PostController.cs
+++ SpaceBlogApi/Apps/Post/PostController.cs
@@ -19,7 +19,6 @@ public class PostController : ControllerBase
 		try
 		{
 			var file = files.FirstOrDefault();
-
 			using var ms = new MemoryStream();
 
 			await file!.CopyToAsync(ms);
@@ -27,6 +26,8 @@ public class PostController : ControllerBase
 			var myArray = ms.ToArray();
 
 			var image64 = Convert.ToBase64String(myArray);
+
+			image64 = $"data:{file!.ContentType};base64,{image64}";
 
 			await PostUseCases.Create(new ()
 			{
