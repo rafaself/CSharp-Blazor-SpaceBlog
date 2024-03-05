ba18fcfbd3c686b53aa3f96623e751c6c63d1837
rafael <rafaondjango@gmail.com>
2024-03-04 23:19:16 -0300

    feat: configurações iniciais do banco de dados

---
+++ SpaceBlogDb/CustomDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpaceBlogDb;
+
+public class CustomDbContext : DbContext
+{
+	
+	public CustomDbContext()
+	{
+
+	}
+	
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		base.OnConfiguring(optionsBuilder);
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+	}
+
+}
